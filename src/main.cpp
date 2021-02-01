967b80032a352dca8803b4ef5f31ca2e90b3cf0a
Robert Eells <eellsrm@gmail.com>
2021-01-31 19:18:27 -0500

    temp good(maybe)

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@
 #include <Wire.h>
 #endif
 
-U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
+U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R2, /* reset=*/ U8X8_PIN_NONE);
 // Create a U8g2log object
 U8G2LOG u8g2log;
 
@@ -25,7 +25,12 @@ int ThermPin = 0;
 int Vo;
 float R1 = 4700;
 float logR2, R2, T;
-float c1 = 1.009249522e-03, c2 = 2.378405444e-04, c3 = 2.019202697e-07;
+//float c1 = 1.009249522e-03, c2 = 2.378405444e-04, c3 = 2.019202697e-07; //10k ntc thermisistor, works with salvage
+//float c1 = 1.125308852122e-03, c2 = 2.34711863267e-04, c3 = 8.5663516e-07; //100k?
+//float c1 = 1.484778004e-03, c2 = 2.348962910e-04, c3 = 1.006037158e-07;  // Steinhart-Hart and Hart Coefficients
+float c1 = 0.8269846691e-03, c2 = 2.088175627e-04, c3 = 0.8055701799e-07;  // another try at 100k, seems to work
+
+
 /*
 */
 
@@ -33,18 +38,11 @@ void setup() {
   Serial.begin(9600);
 
   u8g2.begin();
-/*
-  u8g2.setFont(u8g2_font_tom_thumb_4x6_mf);	// set the font for the terminal window
-
-  u8g2log.begin(u8g2, U8LOG_WIDTH, U8LOG_HEIGHT, u8log_buffer);	// connect to u8g2, assign buffer
-  u8g2log.setLineHeightOffset(0);	// set extra space between lines in pixel, this can be negative
-  u8g2log.setRedrawMode(0);		// 0: Update screen with newline, 1: Update screen for every char  
-  u8g2log.println("lcd setup complete");
-*/
 }
 
 
 void loop() {
+
 
   Vo = analogRead(ThermPin);
   R2 = R1 * (1023.0 / (float)Vo - 1.0);
@@ -52,6 +50,10 @@ void loop() {
   T = (1.0 / (c1 + c2*logR2 + c3*logR2*logR2*logR2));
   T = T - 273.15;
   T = (T * 9.0)/ 5.0 + 32.0; 
+
+  char Tchar[5];
+  dtostrf(T, 4, 2, Tchar);
+
 
   Serial.print("Temperature: "); 
   Serial.print(T);
@@ -61,7 +63,7 @@ void loop() {
   u8g2.clearBuffer();					// clear the internal memory
   u8g2.setFont(u8g2_font_ncenB08_tr);	// choose a suitable font
   u8g2.drawStr(0,10,"temp reading");	// write something to the internal memory
-  //u8g2.drawStr(0,30,T);
+  u8g2.drawStr(60,60,Tchar);
   u8g2.sendBuffer();
 
   delay(500);
